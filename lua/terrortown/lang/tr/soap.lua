61615cc23d6ee9137f0c1d3ea622e4c474644f73
Tora <49911006+NovaDiablox@users.noreply.github.com>
2024-11-16 00:16:56 +0300

    Added Turkish Localization

---
+++ lua/terrortown/lang/tr/soap.lua
@@ -0,0 +1,11 @@
+L = LANG.GetLanguageTableReference("turkish")
+
+L["soap_addon_header"] = "Sabun Ayarları"
+L["weapon_soap_name"] = "Sabun"
+L["weapon_soap_desc"] = "Dikkat et kayma! Üzerine basan teröristleri uçurur."
+L["weapon_soap_destroyed"] = "Sabunun yok edildi!"
+L["soap_help_pri"] = "Sabunu yüzeye yapıştır"
+
+L["soap_help_menu"] = "Bu menü, Blechkanne tarafından oluşturulan sabun eklentisini yapılandırmanızı sağlar"
+L["label_soap_kick_props"] = "Nesnelerin sabunla etkileşime girmesini ister misiniz?"
+L["label_soap_velocity"] = "Bir oyuncuyu uçurma hızı"
